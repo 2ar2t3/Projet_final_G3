cb95cb9f6c44e32747ce9c95e55e761a610a685c
2ar2t3 <arthur.aussedat-pretet.1@ens.etsmtl.ca>
2025-06-11 19:57:51 -0400

    Transformation en dataframe terminée

--- Tubulence/main.py
+++ Tubulence/main.py
@@ -1,4 +1,7 @@
 import threading
+
+from Tools.scripts.parse_html5_entities import get_json
+
 from Requetes_OpenSky import *
 
 class Main:
@@ -6,12 +9,10 @@ class Main:
     def __init__(self, bbox = None):
         """On initialise et on lance un timer à l'appel de la classe"""
         self.timer = None
-
         if not bbox:
             self.bbox = {}
         else:
             self.bbox = bbox
-
         self.start_timer()
 
     def start_timer(self):
@@ -22,9 +23,9 @@ class Main:
 
     def recup_donnees(self):
         """Fonction qui appelle un objet OpenSky() contenant le state array brut"""
-        state = OpenSky().get_json(self.bbox)["states"]
-        self.start_timer()  #On relance le timer dès que la requête est parvenue
-        print(state)
-        return state
 
-main = Main({"lamin" : 45.83, "lomin" : 5.99, "lamax" : 47.82, "lomax" : 10.52})
+        #States est un dataframe
+        states = OpenSky().get_json(self.bbox)
+        self.start_timer()  #On relance le timer dès que la requête est parvenue
+
+main = Main()
